2d64ec43625a1741ca6f4c27007ae9a0960800fc
Okechukwu Chude <52760448+okechukwuchude@users.noreply.github.com>
2024-06-20 18:18:30 +0100

    update

--- app.py
+++ app.py
@@ -2,15 +2,19 @@ import streamlit as st
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.chains.summarize import load_summarize_chain
-from transformers import T5Tokenizer, T5ForConditionalGeneration
+from transformers import T5Tokenizer, T5ForConditionalGeneration, AutoModelForSeq2SeqLM, AutoTokenizer
 from transformers import pipeline
 import torch
 import base64
 
-#load model and tokenizer
-checkpoint = "LaMini-Flan-T5-248M"
-tokenizer = T5Tokenizer.from_pretrained(checkpoint)
-base_model = T5ForConditionalGeneration.from_pretrained(checkpoint, device_map = 'auto', torch_dtype = torch.float32)
+#load model and tokenizer from machine
+# checkpoint = "LaMini-Flan-T5-248M"
+# tokenizer = T5Tokenizer.from_pretrained(checkpoint)
+# base_model = T5ForConditionalGeneration.from_pretrained(checkpoint, device_map = 'auto', torch_dtype = torch.float32)
+
+#load model and tokenizer from huggingface
+tokenizer = AutoTokenizer.from_pretrained("MBZUAI/LaMini-Flan-T5-248M")
+model = AutoModelForSeq2SeqLM.from_pretrained("MBZUAI/LaMini-Flan-T5-248M")
 
 #file loader and preprocessor
 def file_preprocessing(file):
@@ -24,11 +28,25 @@ def file_preprocessing(file):
         final_texts = final_texts + text.page_content
     return final_texts
 
-# model pipeling
+# model pipeling from machine
+# def llm_pipeline(filepath):
+#     pipe_sum = pipeline(
+#         "summarization", 
+#         model = base_model, 
+#         tokenizer = tokenizer,
+#         max_length = 512,
+#         min_length = 50
+#         )
+#     input_text = file_preprocessing(filepath)
+#     result = pipe_sum(input_text)
+#     result = result[0]['summary_text']
+#     return result
+
+# model pipeling from huggingface
 def llm_pipeline(filepath):
     pipe_sum = pipeline(
         "summarization", 
-        model = base_model, 
+        model = model, 
         tokenizer = tokenizer,
         max_length = 512,
         min_length = 50
